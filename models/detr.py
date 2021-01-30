07d6cc89f96d01be370f8efe442a15c4693ab063
Niels Rogge <niels.rogge1@gmail.com>
2021-01-30 10:09:02 +0100

    Add print statements for debugging

--- models/detr.py
+++ models/detr.py
@@ -70,8 +70,8 @@ class DETR(nn.Module):
         assert mask is not None
         hs = self.transformer(self.input_proj(src), mask, self.query_embed.weight, pos[-1])[0]
 
-        # print("Hidden state after transformer:")
-        # print(hs.shape)
+        print("Hidden state after transformer:")
+        print(hs.shape)
 
         outputs_class = self.class_embed(hs)
         outputs_coord = self.bbox_embed(hs).sigmoid()
